23c6895f3e1bbeee09dfa55e33f452a5d1dbf36b
Kazuki Okamoto <kazuki.okamoto@kakkun61.com>
2016-07-28 10:00:21 +0900

    scroll style

--- yi.hs
+++ yi.hs
@@ -1,4 +1,17 @@
 import Yi
+import Yi.Config.Misc
+
+myConfig :: Config
+myConfig = defaultEmacsConfig
+
+myUIConfig :: UIConfig
+myUIConfig =
+  (configUI myConfig)
+    { configScrollStyle = Just SingleLine
+    }
 
 main :: IO ()
-main = yi defaultEmacsConfig
+main =
+  yi $ myConfig
+    { configUI = myUIConfig
+    }
